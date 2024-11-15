db8d5ee4d8d72574f67e30ad03fbc0bbaab6c67b
Tamale Talemwa Collins <51233620+ttcollins@users.noreply.github.com>
2024-11-15 12:17:36 +0300

    Update RunJsonDataLoader.java

--- src/main/java/com/dolal1/runnerz/run/RunJsonDataLoader.java
+++ src/main/java/com/dolal1/runnerz/run/RunJsonDataLoader.java
@@ -12,16 +12,12 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class RunJsonDataLoader implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(RunJsonDataLoader.class);
     private final RunRepository runRepository;
     private final ObjectMapper objectMapper;
-
-    public RunJsonDataLoader(RunRepository runRepository, ObjectMapper objectMapper) {
-        this.runRepository = runRepository;
-        this.objectMapper = objectMapper;
-    }
 
     @Override
     public void run(String... args) throws Exception {
